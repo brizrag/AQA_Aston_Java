42a8301e6907f4d4cb3939352f82a44748d26b2b
Brizrag <brizrag@gmail.com>
2024-02-13 14:30:19 +0300

    Update file

--- compareNumbers.java
+++ compareNumbers.java
@@ -1,2 +1,12 @@
 public class compareNumbers {
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 11;
+        if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a >= b");
+        }
+    }
 }
--- printColor.java
+++ printColor.java
@@ -1,2 +1,14 @@
 public class printColor {
+
+    public static void main(String[] args) {
+
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        } else {
+            System.out.println("Желтый");
+        }
+    }
 }
