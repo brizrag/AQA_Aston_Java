659a9f407b6a3d0eb40afa5b2a2d993dbbc39979
Brizrag <brizrag@gmail.com>
2024-02-13 13:47:02 +0300

    Update file

---
+++ checkSumSign.java
@@ -0,0 +1,13 @@
+public class checkSumSign {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        int sym = a + b;
+        if (sym > 0 ) {
+            System.out.println("Сумма положительная");
+        } else  {
+                System.out.println(("Сумма отрицательная"));
+            }
+    }
+}
