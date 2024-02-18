7fa58699f0eccc7ae4b7b76393c7da7a2e33a2fe
Brizrag <brizrag@gmail.com>
2024-02-18 13:37:14 +0300

    Modify file Lesson_2.java

---
+++ Lesson_2.java
@@ -0,0 +1,35 @@
+public class Lesson_2 {
+
+    public static void main(String[] args) {
+//        System.out.println("Orange");
+//        System.out.println("Banana");
+//        System.out.println("Apple");
+
+//        int a = 10;
+//        int b = 4;
+//        int sym = a + b;
+//        if (sym > 0) {
+//            System.out.println("Сумма положительная");
+//        } else {
+//            System.out.println(("Сумма отрицательная"));
+
+//        int value = 10;
+//        if (value <= 0) {
+//            System.out.println("Красный");
+//        } else if (value > 100) {
+//            System.out.println("Зеленый");
+//        } else {
+//            System.out.println("Желтый");
+
+        int a = 9;
+        int b = 11;
+        if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a >= b");
+
+
+        }
+    }
+}
+
