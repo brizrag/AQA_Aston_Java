a1a9d69c8502d931f46222eab300ee115ba97e1e
Andrey S <106678330+brizrag@users.noreply.github.com>
2024-03-20 12:01:09 +0300

    Delete Lesson_4 directory
    Del

--- Lesson_4/Engineer.java
+++
@@ -1,9 +0,0 @@
-package Lesson_4;
-
-public class Engineer {
-    public static void main(String[] args) {
-        Employee employee1 = new Employee("Petrov Sergey Vitalievich", "Engineer",
-                "PSV_E@gmail.com", "892145945", 35_000, 39);
-        System.out.println(employee1);
-    }
-}
--- Lesson_4/ParkInfo.java
+++
@@ -1,7 +0,0 @@
-package Lesson_4;
-
-public class ParkInfo {
-    public static void main(String[] args) {
-        Park.attractionName attractionName1 = new Park().new attractionName();
-    }
-}
